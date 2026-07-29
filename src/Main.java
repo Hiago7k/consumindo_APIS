47c5a551828ae86da0c233f34c8821b4281527d2
Hiago 7k <hiago7k@gmail.com>
2026-07-29 20:05:15 -0300

    usuario busca a comida

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,23 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
 class Main{
     static void main() throws IOException, InterruptedException {
 
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite o nome da comida que queira saber a receita: ");
+        var buscar = leitura.next();
+
+        String endereco = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + buscar;
+                ;
+//https://www.themealdb.com/api/json/v1/1/search.php?s=noodle
         HttpClient client = HttpClient.newHttpClient();
 
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.coingecko.com/api/v3/ping"))
+                .uri(URI.create(endereco))
                 .build();
 
         HttpResponse <String> response = client
