047498bd59c32006236a9cbd5db15366aefd3e6d
Hiago 7k <hiago7k@gmail.com>
2026-07-29 19:56:44 -0300

    estrutura para realizar req

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 class Main{
-    static void main() {
-        System.out.println("Apis");
+    static void main() throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(""))
+                .build();
+
+        HttpResponse <String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
     }
 }
